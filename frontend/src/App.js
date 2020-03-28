feecdd746b823ad4b989e68ec105fe65cd6fea3f
rafaeloliveirachapouto <rafaeloliveira.c@hotmail.com>
2020-03-27 22:52:08 -0300

    Front-end do app finalizado.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,34 @@
+/**
+ * Código comentado é para teste
+ */
+/*
+import React, { useState } from 'react';
+import Header from './Header';
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  function increment() {
+    setCounter(counter + 1);
+  }
+  return (
+    //<Header title="Semana OmniStack" />
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
+  );
+}*/
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css';
+
+//import Logon from './pages/Logon';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //<Logon />
+    <Routes />
   );
 }
 
